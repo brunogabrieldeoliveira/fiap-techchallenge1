af929d8716a2c380d05b4e8ff8990266b90bee56
Bruno Gabriel de Oliveira <brunog@unimed-es.com.br>
2025-04-03 17:09:00 -0300

    commit 3

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
 from flask import Flask, jsonify
+from flask_caching import Cache
 from webscraping import obtemDados, obtemProducao, obtemProcessamento, obtemComercializacao, obtemImportacao, obtemExportacao
+
+# define configurações do cache
+config = {
+    "DEBUG": True,                  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",    # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300    # timeout padrao
+}
 
 # flask --app .\app.py run --debug
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
+
+# timeout cache
+timeout= 300
 
 # producao ok
 @app.route("/producao", methods=['GET'])
+@cache.cached()
 def producao():
     return obtemProducao()
 
 # processamento
 @app.route("/processamento", methods=['GET'])
+@cache.cached()
 def processamento():
     return obtemProcessamento()
 
 # comercializacao ok
 @app.route("/comercializacao", methods=['GET'])
+@cache.cached()
 def comercializacao():
     return obtemComercializacao()
 
 # importacao
 @app.route("/importacao", methods=['GET'])
+@cache.cached()
 def importacao():
     return obtemImportacao()
 
 # exportacao
 @app.route("/exportacao", methods=['GET'])
+@cache.cached()
 def exportacao():
     return obtemExportacao()
 
